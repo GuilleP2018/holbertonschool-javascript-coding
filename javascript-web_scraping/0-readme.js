6efea94b27062b7f8578dfc6c135c57bbf505aca
GuilleP2018 <gpereyo@gmail.com>
2024-01-23 12:08:21 -0400

    pushing everything and 0 complete

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -4,8 +4,8 @@ const fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf8', (err, data) => {
   if (err) {
-    return console.log(err);
+    console.log(err);
+    return;
   }
-  process.stdout.write(data);
-}
-);
+  console.log(data);
+});
